c41da6b794c51d1f73751129ab4c901cc9bd2185
SUMANTHA.N.T.S <sumanthneechadi@gmail.com>
2021-02-28 22:03:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,28 +6,23 @@ import plotly.express as px
 
 def main():
     st.title("Forecasting the Particulate Matter (PM)")
-    
+
     st.sidebar.header('Upload CSV file')
     uploaded_file = st.sidebar.file_uploader("Choose a file")
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file,index_col=0,parse_dates=True)
         model = sm.tsa.statespace.SARIMAX(df,order=(1,0,1),seasonal_order=(1,0,1,24)).fit()
-        
-        #b1, b2, b3 = st.beta_columns(3)
-        if st.sidebar.button('Dataset Detils'): 
+
+        if st.sidebar.button('Dataset Details'):
             
             st.write('Start date : ',df.index[0])
             st.write('End date : ',df.index[-1])
             st.write('Number of records = ',len(df))
-            
-            
             st.write('Sample data')
             st.write(df.head(5))
         
         if st.sidebar.button('Forecast for 24hours'):
             m = []
-            #start = datetime.datetime.strptime("20-04-2018", "%d-%m-%Y")
-            
             start = df.index[-1]
             for j in range(0,24):
                 date_generated = start + datetime.timedelta(hours=j+1)
@@ -39,6 +34,9 @@ def main():
             
             fig = px.line(df_new, x=df_new.index, y = df_new.columns,width =1000, title='Forecasting For next 24 Hours')
             st.plotly_chart(fig)
+
+        if st.sidebar.button('Model Used'):
+            st.subheader('SARIMA(1,0,1,24) model is used for Forecasting')
 
 if __name__=='__main__':
     main()
